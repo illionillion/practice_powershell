62004882d3b5fe7d956f73ca0b54a50042bf75e5
illionillion <yuto.0526.sano@gmail.com>
2021-08-18 22:44:50 +0900

    ファイルのfor分で作成と削除

--- test.ps1
+++ test.ps1
@@ -7,5 +7,7 @@ for ($i=0; $i -lt 10; $i++){
     Write-Output($i%2 -lt 0)#-lt <
     Write-Output($i%2 -gt 0)#-gt >
     Write-Output('gu-su-'+$i)
+    new-Item .\test$i.txt#ファイル作成
+    remove-Item .\test$i.txt#ファイル削除
   }
 }
