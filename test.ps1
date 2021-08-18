e06ba8fe8a5cfcc8537fd0b4b92be53d15c750ca
illionillion <yuto.0526.sano@gmail.com>
2021-08-18 22:22:03 +0900

    演算子は記号ではなく-eqとか

--- test.ps1
+++ test.ps1
@@ -3,6 +3,9 @@
 for ($i=0; $i -lt 10; $i++){
   Write-Output ("count:"+$i)
   if ($i%2 -eq 0) {
+    Write-Output($i%2 -eq 0)#-eq ==
+    Write-Output($i%2 -lt 0)#-lt <
+    Write-Output($i%2 -gt 0)#-gt >
     Write-Output('gu-su-'+$i)
   }
 }
