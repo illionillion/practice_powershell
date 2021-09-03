685bbde8152cef055a57ff92fb90dcfb7c139ce1
illionillion <yuto.0526.sano@gmail.com>
2021-09-03 23:42:20 +0900

    powershellの空の配列の作り方追加の仕方分かった。

--- test.ps1
+++ test.ps1
@@ -1,5 +1,9 @@
 # Set-ExecutionPolicy RemoteSigned -Scope Process -Force
 # このコマンドで権限確保？
+$array=@(); #空の配列
+Write-Output("all:"+$array.Count)
+
+
 for ($i=0; $i -lt 10; $i++){
   Write-Output ("count:"+$i)
   if ($i%2 -eq 0) {
@@ -7,7 +11,11 @@ for ($i=0; $i -lt 10; $i++){
     Write-Output($i%2 -lt 0)#-lt <
     Write-Output($i%2 -gt 0)#-gt >
     Write-Output('gu-su-'+$i)
-    new-Item .\test$i.txt#ファイル作成
-    remove-Item .\test$i.txt#ファイル削除
+    $array+=$i
+    new-Item .\test$i.txt #ファイル作成
+    remove-Item .\test$i.txt #ファイル削除
   }
 }
+
+Write-Output("all:"+$array.Count)
+Write-Output($array)
